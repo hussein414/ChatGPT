db8a2dc8ab121cccc42e5a1f542130d7c15cd576
@Hussein_mj <ehossein54@gmail.com>
2023-05-12 15:03:12 +0330

    nothing

--- app/src/main/java/com/example/chatgpt/utils/Constance.kt
+++ app/src/main/java/com/example/chatgpt/utils/Constance.kt
@@ -1,7 +1,7 @@
 package com.example.chatgpt.utils
 
 object Constance {
-    const val MY_API_KEY="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"
+    const val MY_API_KEY="Your api key"
     const val END_POINT="v1/completions"
     const val BASE_URL="https://api.openai.com/"
     const val SENT_BY_ME = "sent_me"
